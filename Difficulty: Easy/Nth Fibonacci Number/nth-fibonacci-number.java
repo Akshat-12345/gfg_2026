45dcb19fa06502b77ca2d73f50502c22bf7ff726
Akshat Bajpai <143333942+Akshat-12345@users.noreply.github.com>
2026-03-30 11:56:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    int [] dp; 
+    public int nthFibonacci(int n) {
+        // code here
+       dp = new int[n + 1];
+       Arrays.fill(dp,-1);
+       return solve(n);
+    }
+    
+    public int solve(int n){
+       if(n == 1 || n == 0) return n;
+
+       if(dp[n] != -1) return dp[n];
+
+       return dp[n] = solve(n-1) + solve(n-2);
+    }
+}
